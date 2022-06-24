7785846461ed20d001fecb28f0b66a5e529d854f
Ishika1601 <bansalishika9213@gmail.com>
2022-06-24 11:24:17 +0530

    Adds commit

---
+++ app/src/main/java/com/raywenderlich/android/taskie/model/response/CompleteNotesResponse.kt
@@ -0,0 +1,6 @@
+package com.raywenderlich.android.taskie.model.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class CompleteNotesResponse(val message: String?)
---
+++ app/src/main/java/com/raywenderlich/android/taskie/model/response/DeleteNoteResponse.kt
@@ -0,0 +1,6 @@
+package com.raywenderlich.android.taskie.model.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class DeleteNoteResponse(val message: String)
